1f0f800d42ffaee86cfd949c4e3a30b4f8683e02
Michael Callahan <mcalla242@gmail.com>
2023-02-23 15:45:39 -0700

    Add spicy challenge re-do

--- spicy.rb
+++ spicy.rb
@@ -1,5 +1,5 @@
 def fizz_buzz(num)
-    if num % 3 == 0 && num % 5 ==0
+    if num % 3 == 0 && num % 5 == 0
         puts "FizzBuzz"
     elsif num % 3 == 0
         puts "Fizz"
@@ -10,7 +10,7 @@ def fizz_buzz(num)
     end
 end
 
-fizz_buzz(15)
-fizz_buzz(9)
-fizz_buzz(25)
-fizz_buzz(11)
+fizz_buzz(15) #FizzBuzz
+fizz_buzz(9) # Fizz
+fizz_buzz(25) # Buzz
+fizz_buzz(11) # 11 (num)
